6352923abe6b916838b8d86f7343ee97827e0bd2
Haseebz1 <haseebkandore@hotmail.com>
2023-06-27 00:06:46 +0100

    Completed html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,11 @@ const path = require("path");
 const router = require("express").Router();
 
 router.get("/notes", (req, res) => {
-    res.sendFile(path.join ( __dirname, '../public/notes.html'))
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
-module.exports = router 
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const htmlRoutes = require('./routes/htmlRoutes')
 
 const PORT = 3001
+
+app.use('/', htmlRoutes)
 
 
 
