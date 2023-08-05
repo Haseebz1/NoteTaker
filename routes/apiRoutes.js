90f53e65ee8f9886cc36882b1b06ed113e011af0
Haseebz1 <haseebkandore@hotmail.com>
2023-08-05 01:31:41 +0100

    helper function to read and write data changed in routes file and also route has been debuged , helper file is removed

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,76 @@
 const router = require("express").Router();
-const { notes } = require("../db/db.json");
-const {v4: uuidv4} = require('../helper/fsutils');
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require('uuid');
 
+// Helper function to read data from a file
+const readFromFile = (filePath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(JSON.parse(data));
+      }
+    });
+  });
+};
 
-//  readFromFile,
-//   } = require('./helper/fsutils');
-// const { json } = require("express");
+// Helper function to write data to a file
+const writeToFile = (filePath, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filePath, JSON.stringify(data, null, 2), (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+};
 
+// Function to create a new note and add it to the 'notes' array
+const createNewNote = async (note) => {
+  const dbPath = path.join(__dirname, "../db/db.json");
 
+  try {
+    const notesArray = await readFromFile(dbPath);
+
+    const newNote = {
+      id: uuidv4(),
+      title: note.title,
+      text: note.text,
+    };
+
+    notesArray.push(newNote);
+
+    await writeToFile(dbPath, notesArray);
+
+    return newNote;
+  } catch (err) {
+    throw err;
+  }
+};
 
 // GET notes
-router.get("/notes", (req, res) => {
-  let results = notes;
-  res.json(results);
+router.get("/notes", async (req, res) => {
+  const dbPath = path.join(__dirname, "../db/db.json");
+  try {
+    const notesArray = await readFromFile(dbPath);
+    res.json(notesArray);
+  } catch (err) {
+    res.status(500).json({ error: "Unable to get notes." });
+  }
 });
 
-
 // CREATE note
-router.post('/notes', (req, res) => {
-   
-   
-  router.post("/notes", (req, res) => {
-    req.body.id = uuidv4();
-    const newNote = createNewNote(req.body, notes);
+router.post("/notes", async (req, res) => {
+  try {
+    const newNote = await createNewNote(req.body);
     res.json(newNote);
-  });  
+  } catch (err) {
+    res.status(500).json({ error: "Unable to create a new note." });
+  }
 });
 
 // DELETE note bonus
